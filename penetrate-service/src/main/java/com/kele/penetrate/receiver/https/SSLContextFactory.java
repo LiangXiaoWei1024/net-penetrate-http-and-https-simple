7f4692b49ede5ecbcf9bcbc6c9ffa200f3034305
xiaowei <xiaowei8192@163.com>
2022-04-13 20:29:58 +0800

    修改

--- penetrate-service/src/main/java/com/kele/penetrate/receiver/https/SSLContextFactory.java
+++ penetrate-service/src/main/java/com/kele/penetrate/receiver/https/SSLContextFactory.java
@@ -9,11 +9,11 @@ public class SSLContextFactory
 {
     public static SSLContext getSslContext() throws Exception
     {
-        char[] passArray = "你证书的密码".toCharArray();
+        char[] passArray = "证书密码".toCharArray();
         SSLContext sslContext = SSLContext.getInstance("TLSv1");
         KeyStore ks = KeyStore.getInstance("JKS");
         //加载keytool 生成的文件
-        FileInputStream inputStream = new FileInputStream("你证书的路径");
+        FileInputStream inputStream = new FileInputStream("证书路径");
         ks.load(inputStream, passArray);
         KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
         kmf.init(ks, passArray);
